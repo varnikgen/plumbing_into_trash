cc77024ca720a5e82ba3cc0bcd2ba26b67a939c0
Nikolay Varfolomeev <varnikgen@yandex.ru>
2020-07-29 22:04:06 +1000

    Добавлен не самый удачный шаблон для магазина надо будет обдумать

--- plumbing/plumbing/settings.py
+++ plumbing/plumbing/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'plumbing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- plumbing/shop/views.py
+++ plumbing/shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Product
+
+
+class ProductsView(View):
+    """Список товаров"""
+    def get(self, request):
+        product = Product.objects.filter(available=True)
+        return render(request, "shop/products.html", {"product_list": product})
