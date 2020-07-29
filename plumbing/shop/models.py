d88121c0913f6224beb40f7251134f3624faea1f
Nikolay Varfolomeev <varnikgen@yandex.ru>
2020-07-29 19:44:26 +1000

    Initial commit

---
+++ plumbing/shop/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Категории"""
+    name = models.CharField("Категория", max_length=150, db_index=True)
+    url = models.SlugField(max_length=160, db_index=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    """Товар"""
+    category = models.ForeignKey(Category, verbose_name='Товар', on_delete=models.SET_NULL, null=True)
+    name = models.CharField("Наименование", max_length=160, db_index=True)
+    url = models.SlugField(max_length=160, db_index=True)
+    image = models.ImageField("Изображение", upload_to='products/')
+    description = models.TextField("Описание", blank=True)
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField("Количество")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'url'),)
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
